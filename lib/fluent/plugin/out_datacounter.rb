2cc066f5252f2f62ecce8519867d7b7db520a855
Kazuyuki Sakemi <shakeren20@gmail.com>
2014-01-01 02:25:40 +0900

    write skeleton of plugin and its tests

--- lib/fluent/plugin/out_datacounter.rb
+++ lib/fluent/plugin/out_datacounter.rb
@@ -1,5 +1,37 @@
-module Fluent
-	Class DataCounterOutput < Fluent::BufferedOutput
-    # Your code goes here...
-  end
+class Fluent::DataCounterOutput < Fluent::BufferedOutput
+ Fluent::Plugin.register_output('datacounter', self)
+
+	# config_param :hoge, :string, :default => 'hoge'
+
+	def initialize
+		super
+	  # require 'hogepos'
+	end
+
+	def configure(conf)
+		super
+		# @path = conf['path']
+	end
+
+	def start
+		super
+		# init
+	end
+
+	def shutdown
+		super
+		# estroy
+	end
+
+	def format(tag, time, record)
+		[tag, time, record].to_msgpack
+	end
+
+def write(chunk)
+		record = []
+		chunk.msgpack_each do |record|
+			# records << record
+		end
+		# write records
+	end
 end
